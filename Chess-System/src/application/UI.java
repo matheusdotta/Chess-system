da889706c977a56199b12c8d55ce069dadf605b5
Matheus Dotta <matheus_dotta@live.com>
2021-04-27 21:49:28 -0300

    Moving Piece

--- Chess-System/src/application/UI.java
+++ Chess-System/src/application/UI.java
@@ -1,6 +1,11 @@
 package application;
 
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 import chess.ChessPiece;
+import chess.ChessPosition;
 import chess.Color;
 
 public class UI {
@@ -30,6 +35,18 @@ public class UI {
 	public static void clearScreen() {
 		System.out.print("\033[H\033[2J");
 		System.out.flush();
+	}
+	
+	public static ChessPosition readChessPosition(Scanner sc){
+		try {
+		String s = sc.nextLine();
+		char column = s.charAt(0);
+		int row = Integer.parseInt(s.substring(1));
+		return new ChessPosition(column,row);
+		}		
+		catch (RuntimeException e) {
+			throw new InputMismatchException("ERRO Lendo Posição do Xadrez. Valid values are from a1 to H8.");
+		}
 	}
 
 	public static void printBoard(ChessPiece[][] pieces) {
